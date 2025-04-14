f9cc55d66e201d7f3e87f03f4f576510802291b5
Leonardo <leonardo3442@hotmail.com>
2025-04-13 23:41:07 -0300

    test: should not be able to check in twice in the same day

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,18 +1,17 @@
-import 'dotenv/config';
-import { z } from 'zod';
-
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-    PORT: z.coerce.number().default(3333)
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
 });
 
 // validação do schema
-const _env = envSchema.safeParse(process.env) 
+const _env = envSchema.safeParse(process.env);
 
-if(_env.success === false) {
-    console.log('❌ Invalid environment variables', _env.error.format());
-    throw new Error('Invalid environment variables.');
+if (_env.success === false) {
+  console.log("❌ Invalid environment variables", _env.error.format());
+  throw new Error("Invalid environment variables.");
 }
 
 export default _env.data;
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,38 +1,55 @@
-import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { expect, describe, it, beforeEach } from 'vitest'
-import { CheckInUseCase } from '@/use-cases/check-in'
-import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
-import { Decimal } from '@prisma/client/runtime/library'
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { expect, describe, it, beforeEach } from "vitest";
+import { CheckInUseCase } from "@/use-cases/check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
-let checkInsRepository: InMemoryCheckInsRepository
-let gymsRepository: InMemoryGymsRepository
-let sut: CheckInUseCase
+let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
+let sut: CheckInUseCase;
 
-describe('Check-in Use Case', () => {
-    beforeEach(() => {
-        checkInsRepository = new InMemoryCheckInsRepository()
+describe("Check-in Use Case", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository();
 
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository);
+    gymsRepository.items.push({
+      id: "gym-01",
+      title: "JavaScript Gym",
+      description: "",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
+  });
 
-        gymsRepository = new InMemoryGymsRepository()
-        sut = new CheckInUseCase(checkInsRepository, gymsRepository)
-        gymsRepository.items.push({
-            id: 'gym-01',
-            title: 'JavaScript Gym',
-            description: '',
-            phone: '',
-            latitude: new Decimal(0),
-            longitude: new Decimal(0),
-        })
-    })
+  it("should be able to check in", async () => {
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
+    });
 
-    it('should be able to check in', async () => {
-        const { checkIn } = await sut.execute({
-            gymId: 'gym-01',
-            userId: 'user-01',
-            userLatitude: -27.2092052,
-            userLongitude: -49.6401091,
-        })
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
 
-        expect(checkIn.id).toEqual(expect.any(String))
-    })
-})
+  it("should not be able to check in twice in the same day", async () => {
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+        userLatitude: -27.2092052,
+        userLongitude: -49.6401091,
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
