b23e73fd49be7c5dd71fb137b50f475b55329c84
Leonardo <leonardo3442@hotmail.com>
2025-04-18 22:30:16 -0300

    feat: auth user and refresh token

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,14 +1,15 @@
 import { FastifyInstance } from "fastify";
-import { register } from "./controllers/register";
-import { authenticate } from "./controllers/authenticate";
-import { profile } from "./controllers/profile";
-
-
+import { register } from "./controllers/users/register";
+import { authenticate } from "./controllers/users/authenticate";
+import { profile } from "./controllers/users/profile";
+import { verifyJWT } from "./middlewares/verify-jwt";
+import { refresh } from "./controllers/users/refresh";
 
 export async function appRoutes(app: FastifyInstance) {
-    app.post('/users', register);
-    app.post('/sessions', authenticate);
+    app.post("/users", register);
+    app.post("/sessions", authenticate);
+    app.patch("/token/refresh", refresh);
 
     /* Authenticated */
-    app.get('/me', profile)
+    app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
--- src/repositories/prisma/prisma-users-rapository.ts
+++ src/repositories/prisma/prisma-users-rapository.ts
@@ -1,11 +1,16 @@
-import { prisma } from '@/lib/prisma'
-import { Prisma, User } from '@prisma/client';
-import { IUsersRepository } from '../users-repository';
-
+import { prisma } from "@/lib/prisma";
+import { Prisma, User } from "@prisma/client";
+import { IUsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements IUsersRepository {
-    findById(id: string): Promise<User | null> {
-        throw new Error('Method not implemented.');
+    async findById(id: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        return user;
     }
     async findByEmail(email: string) {
         const user = await prisma.user.findUnique({
